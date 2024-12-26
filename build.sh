c50f7aaea1c73a949e1bab2998d9f87659e0e362
April Douglas <aprildouglas@utexas.edu>
2024-12-25 23:12:17 -0600

    module cleanup and failed build automation through .yml

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 pushd ../zmk/app
-
-west build -b nrf5340dk_nrf5340_cpuapp --pristine -S zmk-usb-logging --\
- -DSHIELD=keebo -DZMK_CONFIG="/home/april/Repositories/Keebo/sw/zmk-config" \
- -DZMK_EXTRA_MODULES="/home/april/Repositories/Keebo/sw/zmk-config/module/kscan-custom"
+ 
+ west build -b nrf5340dk_nrf5340_cpuapp --pristine -- \
+ -DSHIELD=keebo \
+ -DZMK_CONFIG=/home/april/Repositories/Keebo/sw/zmk-config \
+ -DZMK_EXTRA_MODULES="/home/april/Repositories/Keebo/sw/zmk-config/module/kscan_custom;/home/april/Repositories/Keebo/sw/zmk-config/module/display" \
+ -DEXTRA_CONF_FILE=/home/april/Repositories/Keebo/sw/zmk-config/module/display/prj.conf \
+ -DEXTRA_DTC_OVERLAY_FILE=/home/april/Repositories/Keebo/sw/zmk-config/module/display/boards/nrf5340dk_nrf5340_cpuapp.overlay
