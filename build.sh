2999754d610d51c605dcd375813cd13745f83bf5
April Douglas <aprildouglas@utexas.edu>
2024-12-26 04:01:22 -0600

    added encoder and LEDs

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 pushd ../zmk/app
  
- west build -b nrf5340dk_nrf5340_cpuapp --pristine -- \
+ west build -b nrf5340dk_nrf5340_cpuapp --pristine -S zmk-usb-logging -- \
  -DSHIELD=keebo \
  -DZMK_CONFIG=/home/april/Repositories/Keebo/sw/zmk-config \
  -DZMK_EXTRA_MODULES="/home/april/Repositories/Keebo/sw/zmk-config/module/kscan_custom;/home/april/Repositories/Keebo/sw/zmk-config/module/display" \
