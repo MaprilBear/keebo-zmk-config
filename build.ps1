05185b390267b04322884a37f53674ae176e6e00
April Douglas <maprilbear@gmail.com>
2025-10-30 23:14:31 -0700

    Moved over to modules, testing zmk 0.4

--- build.ps1
+++ build.ps1
@@ -8,7 +8,7 @@ Push-Location ../zmk/app
 
 west build -b seeeduino_xiao_ble $flags -S cdc-acm-console -- `
  -DSHIELD=keypad -DZMK_CONFIG="C:\Users\April\Documents\Repos\Keebo\sw\zmk-config" `
- -DZMK_EXTRA_MODULES="C:\Users\April\Documents\Repos\Keebo\sw\zmk-config\module\display;C:\Users\April\Documents\Repos\Keebo\sw\zmk-config\module\usb_mass" `
- -DEXTRA_CONF_FILE="C:\Users\April\Documents\Repos\Keebo\sw\zmk-config\module\display\prj.conf; C:\Users\April\Documents\Repos\Keebo\sw\zmk-config\module\usb_mass\prj.conf"
+ -DZMK_EXTRA_MODULES="C:\Users\April\Documents\Repos\Keebo\sw\zmk-modules\display_app;C:\Users\April\Documents\Repos\Keebo\sw\zmk-modules\usb_mass" `
+ -DEXTRA_CONF_FILE="C:\Users\April\Documents\Repos\Keebo\sw\zmk-modules\display_app\prj.conf; C:\Users\April\Documents\Repos\Keebo\sw\zmk-modules\usb_mass\prj.conf"
 
  Pop-Location
--- module/display/src/mini_canvas.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <lvgl.h>
-#include <cstdint>
-
-struct MiniCanvas : public lv_canvas_t
-{
-   std::uint8_t* canvasBuffer{};
-};
