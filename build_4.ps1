fd361f5e88f6f45a10468f96b1880958fcaa9a79
April Douglas <maprilbear@gmail.com>
2025-11-01 14:11:00 -0700

    ZMK 0.4 Migration

--- build_4.ps1
+++ build_4.ps1
@@ -1,8 +1,10 @@
 Push-Location ../zmk4_1/app
 
 west build -b xiao_ble $flags -S cdc-acm-console -- `
- -DSHIELD=keypad -DZMK_CONFIG="C:\Users\April\Documents\Repos\Keebo\sw\zmk-config" `
+ -DSHIELD=keypad `
+ -DZMK_CONFIG="C:\Users\April\Documents\Repos\Keebo\sw\zmk-config" `
  -DZMK_EXTRA_MODULES="C:\Users\April\Documents\Repos\Keebo\sw\zmk-modules\display_app;C:\Users\April\Documents\Repos\Keebo\sw\zmk-modules\usb_mass" `
- -DEXTRA_CONF_FILE="C:\Users\April\Documents\Repos\Keebo\sw\zmk-modules\display_app\prj.conf; C:\Users\April\Documents\Repos\Keebo\sw\zmk-modules\usb_mass\prj.conf"
+ -DEXTRA_CONF_FILE="C:\Users\April\Documents\Repos\Keebo\sw\zmk-modules\display_app\prj.conf; C:\Users\April\Documents\Repos\Keebo\sw\zmk-modules\usb_mass\prj.conf" `
+ -DEXTRA_DTC_OVERLAY_FILE="C:\Users\April\Documents\Repos\Keebo\sw\zmk-modules\display_app\boards\xiao_ble.overlay;C:\Users\April\Documents\Repos\Keebo\sw\zmk-modules\usb_mass\boards\xiao_ble.overlay"
 
- Pop-Location
+Pop-Location
---
+++ flash_4.ps1
@@ -0,0 +1,2 @@
+# docker cp zmk-dev:/workspaces/zmk/app/build/zephyr/zmk.uf2 F:/
+Move-Item ..\zmk4_1\app\build\zephyr\zmk.uf2 F:\
