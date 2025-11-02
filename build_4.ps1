82e453fc6b0e71d4e34a5e5a89123803989a5085
April Douglas <maprilbear@gmail.com>
2025-11-01 17:51:20 -0700

    Fixed build script

--- build_4.ps1
+++ build_4.ps1
@@ -1,6 +1,8 @@
+param([string]$flags="")
+
 Push-Location ../zmk4_1/app
 
-west build -b xiao_ble $flags -S cdc-acm-console -- `
+west build $flags -b xiao_ble -S cdc-acm-console -- `
  -DSHIELD=keypad `
  -DZMK_CONFIG="C:\Users\April\Documents\Repos\Keebo\sw\zmk-config" `
  -DZMK_EXTRA_MODULES="C:\Users\April\Documents\Repos\Keebo\sw\zmk-modules\display_app;C:\Users\April\Documents\Repos\Keebo\sw\zmk-modules\usb_mass" `
